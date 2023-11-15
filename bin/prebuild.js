68de53fe4f48eeffe88f3a268b1ae31081d1babf
Geert Plaisier <geert.plaisier@gnamic.nl>
2023-11-15 14:03:05 +0100

    Changed writing version.json to post-build

--- bin/prebuild.js
+++ bin/prebuild.js
@@ -1,19 +1,14 @@
 const chalk = require('chalk');
-const git = require('git-describe');
 const path = require('path');
 const fs = require('fs');
 
-const {version: appVersion} = require('../package.json');
+const { version: appVersion } = require('../node_modules/@tailormap-viewer/core/package.json');
 
 try {
-  const info = git.gitDescribeSync({longSemver: true, customArguments: ['--abbrev=40']});
-  fs.mkdirSync(path.join(__dirname, '../generated/'), {recursive: true});
-  const file = path.resolve(__dirname, '../generated/', 'version.json');
-
-  const version = JSON.stringify({version: appVersion, buildDate: Date(), gitInfo: info}, null, 2);
-
+  const file = path.resolve(__dirname, '../dist/app/', 'version.json');
+  const version = JSON.stringify({version: appVersion, buildDate: Date()}, null, 2);
   fs.writeFileSync(file, version, {encoding: 'utf-8'});
-  console.log(chalk.green(`Wrote version info ${appVersion}, ${info.raw} to ${path.relative(path.resolve(__dirname, '..'), file)}`));
+  console.log(chalk.green(`Wrote version info ${appVersion} to ${path.relative(path.resolve(__dirname, '..'), file)}`));
 } catch(e) {
   console.log(chalk.red('Error writing version and git info'), e);
 }
