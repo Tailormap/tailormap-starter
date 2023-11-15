4334e0bc71025e59e636e41422778859fbfc57cc
Matthijs Laan <matthijsln@gmail.com>
2023-11-15 18:48:07 +0100

    Support Docker build arguments to install Angular libraries, to easily build a customized Tailormap with one or more additional components from such libraries

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ ARG API_VERSION=snapshot
 FROM node:18.17.1 AS builder
 
 ARG BASE_HREF=/
+ARG ADD_NG_LIBRARIES
+ARG APPEND_NPMRC
 
 WORKDIR /app
 
@@ -13,6 +15,7 @@ RUN npm install
 
 COPY . /app
 
+RUN node ./bin/add-ng-libraries.js
 RUN npm run build-localized -- --base-href=${BASE_HREF}
 
 FROM --platform=$BUILDPLATFORM ghcr.io/b3partners/tailormap-api:${API_VERSION}
---
+++ bin/add-ng-libraries.js
@@ -0,0 +1,27 @@
+// Script to be called from Dockerfile to add one or more Angular libraries as specified by a build-time argument.
+// The Angular libraries can contain an extra Tailormap component such as https://github.com/B3Partners/tailormap-hello-world
+// This script basically runs `ng add` for each
+
+const { execSync } = require('child_process');
+const { readFileSync, writeFileSync } = require('fs');
+
+// Append text to .npmrc, to allow mapping prefixes to a specific registry
+const appendNpmRc = process.env['APPEND_NPMRC'];
+if (appendNpmRc) {
+  writeFileSync('.npmrc', readFileSync('.npmrc') + '\n' + appendNpmRc);
+}
+
+const addNgLibraries = process.env['ADD_NG_LIBRARIES'];
+if (addNgLibraries) {
+  addNgLibraries.split(' ').forEach(library => {
+    console.log('Adding Angular library: ' + library);
+    try {
+      const output = execSync('npx ng add --skip-confirmation ' + library);
+      console.log(output.toString());
+    } catch(error) {
+      // Stderr from execSync() is already piped to stderr, so error is output already
+      console.error('Error installing library');
+      process.exit(1);
+    }
+  });
+}
