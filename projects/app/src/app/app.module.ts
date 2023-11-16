9aa45a4c085be8245e1d688163c7d159a36a32af
Geert Plaisier <geert.plaisier@gnamic.nl>
2023-11-16 13:58:04 +0100

    Updated tailormap packages

--- projects/app/src/app/app.module.ts
+++ projects/app/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CoreModule, CoreRoutingModule } from "@tailormap-viewer/core";
+import { CoreModule } from "@tailormap-viewer/core";
 import { SharedModule } from "@tailormap-viewer/shared";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { environment } from '../environments/environment';
@@ -18,7 +18,6 @@ import { environment } from '../environments/environment';
       production: environment.production,
       viewerBaseUrl: environment.viewerBaseUrl,
     }),
-    CoreRoutingModule,
     SharedModule,
     ...environment.imports,
   ],
