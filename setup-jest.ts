e6e1310986948c2a47cd828971f0b3f07d3e394a
Matthijs Laan <matthijsln@gmail.com>
2025-03-21 13:46:20 +0100

    Upgrade to 11.5.0

--- setup-jest.ts
+++ setup-jest.ts
@@ -1,8 +1,15 @@
-import 'jest-preset-angular/setup-jest';
+import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
 import '@angular/localize/init';
 import './projects/app/src/polyfills';
 import '@testing-library/jest-dom';
 import failOnConsole from 'jest-fail-on-console';
 
-failOnConsole();
+setupZoneTestEnv();
+
+// Error is thrown because the JSDOM version Jest uses does not support @layer css construct, ignore for now
+const allowedErrors = ['Could not parse CSS stylesheet'];
+failOnConsole({
+  silenceMessage: (msg) => allowedErrors.some(err => msg.includes(err)),
+});
+
 
