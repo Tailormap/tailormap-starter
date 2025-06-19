a97668398119205198a32a8db9230e5c6c26cb9b
Matthijs Laan <matthijsln@gmail.com>
2025-06-19 15:24:49 +0200

    Update from tailormap-viewer

--- setup-jest.ts
+++ setup-jest.ts
@@ -2,6 +2,7 @@ import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
 import '@angular/localize/init';
 import './projects/app/src/polyfills';
 import '@testing-library/jest-dom';
+import { TextEncoder, TextDecoder } from 'util';
 import failOnConsole from 'jest-fail-on-console';
 
 setupZoneTestEnv();
@@ -12,4 +13,37 @@ failOnConsole({
   silenceMessage: (msg) => allowedErrors.some(err => msg.includes(err)),
 });
 
+global.TextEncoder = TextEncoder;
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+global.TextDecoder = TextDecoder;
 
+window.ResizeObserver =
+  window.ResizeObserver ||
+  jest.fn().mockImplementation(() => ({
+    disconnect: jest.fn(),
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+  }));
+
+window.EventSource = window.EventSource || jest.fn().mockImplementation(() => ({
+  close: jest.fn(),
+}));
+
+Element.prototype.scrollTo = Element.prototype.scrollTo || (() => {});
+
+jest.mock('jsts/org/locationtech/jts/io', () => ({
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  OL3Parser: class MockedParser {
+    public inject() {/*empty*/}
+    public read(input: any) { return input; }
+    public write(input: any) { return input; }
+  },
+}));
+
+jest.mock('jsts/org/locationtech/jts/operation/buffer', () => ({
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  BufferOp: {
+    bufferOp: (input: any) => input,
+  },
+}));
